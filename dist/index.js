98897fec39c7aed7a2451c3c65774f95327c27aa
Uwe <uriegel@hotmail.de>
2023-02-06 10:07:17 +0100

    no dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-import './Menubar.css';
-import { SubMenu } from './SubMenu';
-const Menubar = ({ items }) => {
-    const [selectedIndex, setSelectedIndex] = useState(-1);
-    return (React.createElement("ul", { className: "mbr--menubar" }, items.map((n, i) => SubMenu({ name: n.name, index: i }))));
-};
-export default Menubar;
