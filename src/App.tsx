033dc3fb91646f8b381c67062ea4a3b92ea95f1c
Uwe <uriegel@hotmail.de>
2023-02-07 09:18:27 +0100

    Tabs

--- src/App.tsx
+++ src/App.tsx
@@ -26,103 +26,103 @@ const App = () => {
 			<Menubar items={[{ 
 				name: "_Datei",
 				items: [{
-					name: "_Umbenennen",
-					type: MenuItemType.MenuItem,
-					shortcut: "F2"
-				}, {
-					name: "Er_weitertes Umbenennen",
-					type: MenuItemType.MenuItem,
-					shortcut: "Strg+F2"
-				}, { type: MenuItemType.Separator 
-				}, {
-					name: "_Kopieren",
-					type: MenuItemType.MenuItem,
-					shortcut: "F5"
-				}, {
-					name: "_Verschieden",
-					type: MenuItemType.MenuItem,
-					shortcut: "F6"
-				}, {
-					name: "_Löschen",
-					type: MenuItemType.MenuItem,
-					shortcut: "Ent"
-				}, { type: MenuItemType.Separator 						
-				}, {
-					name: "_Ordner anlegen",
-					type: MenuItemType.MenuItem,
-					shortcut: "F7"
-				}, {
-					name: "_Eigenschaften",
-					type: MenuItemType.MenuItem,
-					shortcut: "Strg+Enter"
-				}, {
-					name: "Öffnen_mit",
-					type: MenuItemType.MenuItem,
-					shortcut: "Alt+Enter"
-				}, { type: MenuItemType.Separator 						
-				}, {
-					name: "_Beenden",
-					type: MenuItemType.MenuItem,
-					key: "END",
-					shortcut: "Alt+F4"
-				}]
+						name: "_Umbenennen",
+						type: MenuItemType.MenuItem,
+						shortcut: "F2"
+					}, {
+						name: "Er_weitertes Umbenennen",
+						type: MenuItemType.MenuItem,
+						shortcut: "Strg+F2"
+					}, { type: MenuItemType.Separator 
+					}, {
+						name: "_Kopieren",
+						type: MenuItemType.MenuItem,
+						shortcut: "F5"
+					}, {
+						name: "_Verschieden",
+						type: MenuItemType.MenuItem,
+						shortcut: "F6"
+					}, {
+						name: "_Löschen",
+						type: MenuItemType.MenuItem,
+						shortcut: "Ent"
+					}, { type: MenuItemType.Separator 						
+					}, {
+						name: "_Ordner anlegen",
+						type: MenuItemType.MenuItem,
+						shortcut: "F7"
+					}, {
+						name: "_Eigenschaften",
+						type: MenuItemType.MenuItem,
+						shortcut: "Strg+Enter"
+					}, {
+						name: "Öffnen_mit",
+						type: MenuItemType.MenuItem,
+						shortcut: "Alt+Enter"
+					}, { type: MenuItemType.Separator 						
+					}, {
+						name: "_Beenden",
+						type: MenuItemType.MenuItem,
+						key: "END",
+						shortcut: "Alt+F4"
+					}]
 			}, { 
 				name: "_Navigation",
 				items: [{
-					name: "_Favoriten",
-					type: MenuItemType.MenuItem,
-					shortcut: "F1"
-				}, {
-					name: "_Gleichen Ordner öffnen",
-					type: MenuItemType.MenuItem,
-					shortcut: "F9"
-				}] 
-			}, { 
-				name: "_Selektion",
-				items: [{
-					name: "_Alles",
-					type: MenuItemType.MenuItem,
-					shortcut: "Num+"
-				}, {
-					name: "_Selektion entfernen",
-					type: MenuItemType.MenuItem,
-					shortcut: "Num-"
-				}] 
-			}, { 
-				name: "_Ansicht",
-				items: [{
-					name: "_Versteckte Dateien",
-					checked: isHidden,
-					setChecked: setIsHidden,
-					type: MenuItemType.MenuCheckItem,
-					shortcut: "Strg+H"
-				}, {
-					name: "_Aktualisieren",
-					type: MenuItemType.MenuItem,
-					shortcut: "Strg+R"
-				}, { type: MenuItemType.Separator 						
+						name: "_Favoriten",
+						type: MenuItemType.MenuItem,
+						shortcut: "F1"
+					}, {
+						name: "_Gleichen Ordner öffnen",
+						type: MenuItemType.MenuItem,
+						shortcut: "F9"
+					}] 
 				}, { 
-					name: "_Vorschau",
-					type: MenuItemType.MenuItem,
-					shortcut: "F3"
-				}, { type: MenuItemType.Separator 						
+					name: "_Selektion",
+					items: [{
+						name: "_Alles",
+						type: MenuItemType.MenuItem,
+						shortcut: "Num+"
+					}, {
+						name: "_Selektion entfernen",
+						type: MenuItemType.MenuItem,
+						shortcut: "Num-"
+					}] 
 				}, { 
-					name: "Dunkles Thema",
-					type: MenuItemType.MenuItem
-				}, { type: MenuItemType.Separator 
-				}, {
-					name: "_Zoomlevel",
-					type: MenuItemType.MenuItem
-				}, {
-					name: "_Vollbild",
-					type: MenuItemType.MenuItem,
-					shortcut: "F11"
-				}, { type: MenuItemType.Separator 												
-				}, { 
-					name: "_Entwicklerwerkzeuge",
-					type: MenuItemType.MenuItem,
-					shortcut: "F12"
-				}] 
+					name: "_Ansicht",
+					items: [{
+						name: "_Versteckte Dateien",
+						checked: isHidden,
+						setChecked: setIsHidden,
+						type: MenuItemType.MenuCheckItem,
+						shortcut: "Strg+H"
+					}, {
+						name: "_Aktualisieren",
+						type: MenuItemType.MenuItem,
+						shortcut: "Strg+R"
+					}, { type: MenuItemType.Separator 						
+					}, { 
+						name: "_Vorschau",
+						type: MenuItemType.MenuItem,
+						shortcut: "F3"
+					}, { type: MenuItemType.Separator 						
+					}, { 
+						name: "Dunkles Thema",
+						type: MenuItemType.MenuItem
+					}, { type: MenuItemType.Separator 
+					}, {
+						name: "_Zoomlevel",
+						type: MenuItemType.MenuItem
+					}, {
+						name: "_Vollbild",
+						type: MenuItemType.MenuItem,
+						shortcut: "F11"
+					}, { type: MenuItemType.Separator 												
+					}, { 
+						name: "_Entwicklerwerkzeuge",
+						type: MenuItemType.MenuItem,
+						shortcut: "F12"
+					}] 
 			}]} />
 			<div> 
 			<h1>Menubar</h1>
